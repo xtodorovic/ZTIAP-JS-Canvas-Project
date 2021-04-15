f1a8b464b11ccc8d7ee8307ff1d7f7ae54982f3f
Miroslav Todorovic <mtodorovic81@gmail.com>
2021-04-15 16:47:48 +0200

    Added rotation for the enemy tanks

--- javaScript/Enemy.js
+++ javaScript/Enemy.js
@@ -5,39 +5,43 @@ function enemy(width, height, x, y, type)
     this.height = height;
     this.speedX = 0;
     this.speedY = 0;
-    this.angle = 1;
+    this.angle = 3;
     this.x = x;
     this.y = y;
     this.type = type;
     this.draw = function(){
         canvas = document.getElementById("myCanvas");
         ctx = canvas.getContext("2d");
-        var enemy1 = document.getElementById("enemy1");
-      /*  switch(this.angle)
+        var enemy1img;
+        switch(this.angle)
         {
-            case 1: tankImg = document.getElementById("tank1"); break;
-            case 2: tankImg = document.getElementById("tank2"); break;
-            case 3: tankImg = document.getElementById("tank3"); break;
-            case 4: tankImg = document.getElementById("tank4"); break;
+            case 1: enemy1img = document.getElementById("enemy1"); break;
+            case 2: enemy1img = document.getElementById("enemy2"); break;
+            case 3: enemy1img = document.getElementById("enemy3"); break;
+            case 4: enemy1img = document.getElementById("enemy4"); break;
             default: break;
-        }*/
+        }
 
-        ctx.drawImage(enemy1, this.x, this.y, 50, 50);
+        ctx.drawImage(enemy1img, this.x, this.y, 50, 50);
     }
     this.direction = function(){
         let direction = getRandomInt(5);
             if(direction == 1){
                 this.speedX = 0;
                 this.speedY = -2;
+                this.angle = 1;
             }else if (direction == 2){
                 this.speedY = 0;
                 this.speedX = 2;
+                this.angle = 2;
             }else if (direction == 3){
                 this.speedY = 2;
                 this.speedX = 0;
+                this.angle = 3;
             }else if (direction == 4){
                 this.speedY = 0;
                 this.speedX = -2;
+                this.angle = 4;
             }
             else{
                 this.speedX = 0;
