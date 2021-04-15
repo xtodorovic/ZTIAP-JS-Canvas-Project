614f47006f092b90f8b172fe60b3ef82177a746e
Miroslav Todorovic <mtodorovic81@gmail.com>
2021-04-15 16:23:36 +0200

    Seperated Player class to a different file

---
+++ javaScript/Player.js
@@ -0,0 +1,41 @@
+
+function Player(width, height, x, y) {
+    this.width = width;
+    this.height = height;
+    this.fireRate = 0;
+    this.speedX = 0;
+    this.speedY = 0; 
+    this.angle = 1;   
+    this.x = x;
+    this.y = y; 
+    
+    this.draw = function(){
+        canvas = document.getElementById("myCanvas");
+        ctx = canvas.getContext("2d");
+        var tankImg;
+        switch(this.angle)
+        {
+            case 1: tankImg = document.getElementById("tank1"); break;
+            case 2: tankImg = document.getElementById("tank2"); break;
+            case 3: tankImg = document.getElementById("tank3"); break;
+            case 4: tankImg = document.getElementById("tank4"); break;
+            default: break;
+        }
+
+        ctx.drawImage(tankImg, this.x, this.y, 50, 50);            
+    }
+    this.newPos = function() {
+        // movements
+        let newx = this.x + this.speedX;
+        let newy = this.y + this.speedY;
+
+        if (newx >= 0 && this.width + newx <= gameAreaWidth)
+        {
+            this.x = newx;
+        }
+        if (newy > 0 && this.height + newy <= gameAreaHeight)
+        {
+            this.y = newy;
+        }
+    }
+}
